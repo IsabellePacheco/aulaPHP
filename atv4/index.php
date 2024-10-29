a9791dc268b389739e047e0d1ccfdb33f02757b9
Isabelle Pacheco <isapachecosenai@gmail.com>
2024-10-29 16:13:52 -0300

    Atividade 4

--- atv4/index.php
+++ atv4/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>O seu nome</title>
+     <title>O seu nome </title>
 </head>
 <body>
     <h1> Digite o seu nome </h1>
